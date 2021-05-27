aae2c8d3415626309d00704b25530bce87de75e6
Beksultan Maukenov <bmaukenov@gmail.com>
2021-05-27 15:21:03 +0600

    Update Settings.vue

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -52,7 +52,23 @@
               :options="entrances"
               class="col-4"
           />
-
+          <CSelect
+              horizontal
+              label="День недели ТО"
+              :value.sync="setting.repairday"
+              :options="entrances"
+              class="col-4"
+          />          
+          <CSelect
+              horizontal
+              label="Время ТО"
+              :value.sync="setting.repairtime"
+              :options="entrances"
+              class="col-4"
+          />           
+            <CButton color="success" type="submit">
+              Сохранить
+            </CButton>
 
         </CCardBody>
       </CCard>
@@ -80,6 +96,8 @@ export default {
       buildings: [{value: 1, label: 'Гагарин Парк'}, {value: 2, label: 'Водно-Зеленый бульвар'}],
       houses: [{value: 1, label: '271'}, {value: 2, label: '12/2'}],
       entrances: [{value: 1, label: '1'}, {value: 2, label: '2'}],
+      repairday: [{value: 1, label: 'Пн'}, {value: 2, label: 'Вт'}, {value: 3, label: 'Ср'}, {value: 4, label: 'Чт'}, {value: 5, label: 'Пт'}, {value: 6, label: 'Сб'}, {value: 7, label: 'Вс'}],
+      repairtime: [{value: 1, label: 'Пн'}, {value: 2, label: '00:00 - 02:00'}, {value: 3, label: '02:00 - 04:00'}, {value: 4, label: '04:00 - 06:00'}, {value: 5, label: '06:00 - 08:00'}],     
 
     };
   },
